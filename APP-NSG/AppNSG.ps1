f45022b64a1fcf2bde9d08dac5c0e78a03228d1b
gouuri <gouuri.sivam@gmail.com>
2019-11-13 15:14:53 +0530

    Application NSG
    we have done NSG list now working in save as CSV

---
+++ APP-NSG/AppNSG.ps1
@@ -0,0 +1,17 @@
+﻿Connect-AzureRmAccount
+$subscriptions=Get-AzureRMSubscription
+
+ForEach ($vsub in $subscriptions){
+Select-AzureRmSubscription $vsub.SubscriptionID
+
+Write-Host
+
+Write-Host “Working on “ $vsub
+
+Write-Host
+
+Get-AzureRmApplicationSecurityGroup | Format-Table Name,ResourceGroupName,Location | ConvertTo-Csv
+
+
+}
+
