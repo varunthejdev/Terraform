df02361d57141c2075e08778e72898765dbe5545
gouuri <gouuri.sivam@gmail.com>
2020-01-03 16:54:04 +0530

    Creating a rule in NSG using terraform

---
+++ Creating_NSG_rule.tf
@@ -0,0 +1,21 @@
+# Configure the Microsoft Azure Provider
+#Gowri_LLmzeyMXu
+provider "azurerm" {
+        subscription_id = "7824d53a-XXXX-42fd-a57a-30a574015b43"
+        client_id       = "XXXXXX-c307-4ce2-8ed3-fa2d11fe4390"
+        client_secret   = "cTwSS.AypS*4J2.LmAzA6Eaiw/zko8BF"
+        tenant_id       = "b1a7c303-0faf-4874-a764-0eb184efd96e"
+}
+resource "azurerm_network_security_rule" "elansg" {
+  name                        = "test123"
+  priority                    = 100
+  direction                   = "Outbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = "portalapiazure"
+  network_security_group_name = "gouuri"
+}
