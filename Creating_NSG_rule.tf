2f9dc59e0919f41efb35a3f30cc03386487e9dfb
gouuri <gouuri.sivam@gmail.com>
2020-01-03 17:03:43 +0530

    Update Creating_NSG_rule.tf

--- Creating_NSG_rule.tf
+++ Creating_NSG_rule.tf
@@ -1,12 +1,12 @@
 # Configure the Microsoft Azure Provider
 #Gowri_LLmzeyMXu
 provider "azurerm" {
-        subscription_id = "7824d53a-XXXX-42fd-a57a-30a574015b43"
-        client_id       = "XXXXXX-c307-4ce2-8ed3-fa2d11fe4390"
-        client_secret   = "cTwSS.AypS*4J2.LmAzA6Eaiw/zko8BF"
+        subscription_id = "7824d53a-63c4-42fd-a57a-30a574015b43"
+        client_id       = "087fb653-c307-4ce2-8ed3-fa2d11fe4390"
+        client_secret   = "cTwVV.AypS*4J2.LmAzA6Eaiw/zko8BF"
         tenant_id       = "b1a7c303-0faf-4874-a764-0eb184efd96e"
 }
-resource "azurerm_network_security_rule" "elansg" {
+resource "azurerm_network_security_rule" "gsp" {
   name                        = "test123"
   priority                    = 100
   direction                   = "Outbound"
@@ -17,5 +17,5 @@ resource "azurerm_network_security_rule" "elansg" {
   source_address_prefix       = "*"
   destination_address_prefix  = "*"
   resource_group_name         = "portalapiazure"
-  network_security_group_name = "gouuri"
+  network_security_group_name = "gsp"
 }
